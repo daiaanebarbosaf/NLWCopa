e11e6e827442c46990d7ec0677d4a9cb48d10ea1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2022-11-03 07:32:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ document.querySelector("#app").innerHTML = `
     <img src="./assets/logo.svg" alt="Logo da NLW Copa" />
   </header>
   <main id="cards">
-        <div class="card">
-          <h2>24/11 <span>quinta</span></h2>
-          <ul>
-            <li>
-              <img src="./assets/icon-brasil.svg" alt="Bandeira do Brasil" />
-              <strong>16:00</strong>
-              <img src="./assets/icon-serbia.svg" alt="Bandeira da Serbia" />
-            </li>
-          </ul>
-        </div>
-      </main>
+    <div class="card">
+      <h2>24/11 <span>quinta</span></h2>
+      <ul>
+        <li>
+          <img src="./assets/icon-brasil.svg" alt="Bandeira do Brasil" />
+          <strong>16:00</strong>
+          <img src="./assets/icon-serbia.svg" alt="Bandeira da Serbia" />
+        </li>
+      </ul>
+    </div>
+  </main>
 `
