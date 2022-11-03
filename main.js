210fea8f4748cee86bb8bb3ed1f47486007378c6
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2022-11-03 08:05:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-      <img src="./assets/icon-${player1}.svg" alt="Bandeira do Brasil" />
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
 
       <strong>16:00</strong>
 
